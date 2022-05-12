ddc6c90990c31a9174828ba7a750e62354698ad2
weiyou1233 <wei.you@qq.com>
2022-05-12 21:16:46 +0800

    serviceWork

--- web/sw.js
+++ web/sw.js
@@ -2,85 +2,83 @@ let CACHE_VERSION = 4;
 let CACHE_NAME = 'cache_v' + CACHE_VERSION
 let CACHE_URLS = [
   '/',
-  '/views/home.html'
-  // 'https://unpkg.com/axios/dist/axios.min.js'
 ]
 
 
-function precache() {
-  return caches
-    .open(CACHE_NAME)
-    .then(cache => {
-      return cache.addAll(CACHE_URLS)
-    })
-}
+// function precache() {
+//   return caches
+//     .open(CACHE_NAME)
+//     .then(cache => {
+//       return cache.addAll(CACHE_URLS)
+//     })
+// }
 
-function clearCache() {
-  return caches.keys().then(keys => {
-    keys.forEach(key => {
-      if (key != CACHE_NAME) {
-        caches.delete(key)
-      }
-    });
-  })
-}
+// function clearCache() {
+//   return caches.keys().then(keys => {
+//     keys.forEach(key => {
+//       if (key != CACHE_NAME) {
+//         caches.delete(key)
+//       }
+//     });
+//   })
+// }
 
-function saveToCache(req, res) {
-  return caches
-    .open(CACHE_NAME)
-    .then(cache => cache.put(req, res))
+// function saveToCache(req, res) {
+//   return caches
+//     .open(CACHE_NAME)
+//     .then(cache => cache.put(req, res))
   
-}
+// }
 
-function fetchAndCache(req) {
-  return fetch(req).then(res => {
-    saveToCache(req, res.clone())
-    return res
-  })
-}
+// function fetchAndCache(req) {
+//   return fetch(req).then(res => {
+//     saveToCache(req, res.clone())
+//     return res
+//   })
+// }
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    precache().then(self.skipWaiting)
-  );
-})
+// self.addEventListener('install', e => {
+//   e.waitUntil(
+//     precache().then(self.skipWaiting)
+//   );
+// })
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    Promise.all([
-      clearCache(),
-      self.clients.claim()
+// self.addEventListener('activate', e => {
+//   e.waitUntil(
+//     Promise.all([
+//       clearCache(),
+//       self.clients.claim()
 
-    ])
-  )
-})
-
-
-self.addEventListener('fetch', e => {
-
-  let url = new URL(e.request.url);
-  if (url.origin !== self.origin) {
-    return
-  }
-
-  if (e.request.url.includes('http://localhost:3000/getStories')) {
-    e.respondWith(
-      fetchAndCache(e.request).catch(() => {
-        return caches.match(e.request)
-      })
-    )
-    return
-  }
-
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request)
-    })
-  );
-});
+//     ])
+//   )
+// })
 
 
-/*
+// self.addEventListener('fetch', e => {
+
+//   let url = new URL(e.request.url);
+//   if (url.origin !== self.origin) {
+//     return
+//   }
+
+//   if (e.request.url.includes('http://localhost:3000/getStories')) {
+//     e.respondWith(
+//       fetchAndCache(e.request).catch(() => {
+//         return caches.match(e.request)
+//       })
+//     )
+//     return
+//   }
+
+//   e.respondWith(
+//     fetch(e.request).catch(() => {
+//       return caches.match(e.request)
+//     })
+//   );
+// });
+
+//////////////////////////////////////////
+
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -107,7 +105,6 @@ self.addEventListener('activate', e => {
     ])
   )
 })
-
 
 self.addEventListener('fetch', e => {
 
@@ -139,5 +136,3 @@ self.addEventListener('fetch', e => {
     })
   );
 });
-
- */
